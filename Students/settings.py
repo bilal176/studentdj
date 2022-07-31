cc37f126c4d7c14c4cd1dddc401e153dbf9ac17d
Bilal Ahmad <93677817+bilal176@users.noreply.github.com>
2022-07-31 10:02:16 +0500

    Update settings.py

--- Students/settings.py
+++ Students/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-98^81h-&t&(e1hw7q41+gw&nybm66pqa=v2y@#emd+_&tb%q87
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Students.herokuapp.com']
 
 
 # Application definition
